c5929d91c145cbf963c6e7aef7df7028ee254b03
wiktor <wiktorkujawa1993@gmail.com>
2021-01-07 00:46:39 +0100

    Added passport-local authentication module and route local auth methods

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,71 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-require('../modules/auth');
+require('../modules/auth/google')(passport);
 
+
+const isValidUser = (req,res,next) => {
+  if(req.isAuthenticated()) next();
+  else return res.status(401).json({message:"You're not logged in"});
+}
+
+
+// Local authentication and register
+
+const register = async (req, res) => {
+
+  const { displayName, email, password, password2} = req.body;
+  let errors = [];
+  if (!displayName || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  if (undefined !== password && password.length < 8) {
+    errors.push({ msg: 'Password must be at least 8 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.status(400).json(errors);
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+
+        res.status(409).json(errors);
+      } else {
+          const user = new User({
+            email: email,
+            displayName: displayName,
+            password: User.hashPassword(password)
+          }).save();
+          return res.status(201).json({msg: 'Resgistration success'});
+        }
+      })
+    }
+}
+
+router.post('/register', (req, res, next) => {
+  register(req, res);
+});
+
+
+
+router.post('/login', (req,res,next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) { return res.status(501).json(err); }
+    if (!user) { return res.status(501).json(info); }
+    req.logIn(user, function(err) {
+      if (err) { return res.status(501).json(err); }
+      return res.status(200).json({message:'Login Success'});
+    });
+  })(req, res, next);
+});
+
+// Google authentication
 router.get('/google', passport.authenticate('google', { scope: ['profile','email'] }));
 router.get('/google/callback', passport.authenticate('google', {
   successRedirect: '/',
@@ -10,12 +73,11 @@ router.get('/google/callback', passport.authenticate('google', {
 }));
 
 
-router.get('/user', (req,res)=>{ 
-  res.send(req.user)
+router.get('/user', isValidUser, (req,res)=>{ 
+  return res.status(200).json(req.user);
 });
- 
 
-router.get("/logout", async (req, res) => {
+router.get("/logout", isValidUser, async (req, res) => {
   await req.logout();
   res.redirect('/');
 });
